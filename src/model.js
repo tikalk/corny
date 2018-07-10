82c0b5d380fa1aea6a31c2b8d1603f909434694b
alonronin <alon@ronin.co.il>
2018-07-10 16:48:09 +0300

    predict, lodash

--- src/model.js
+++ src/model.js
@@ -2,9 +2,16 @@ const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 
 const { resolve } = require('path');
+const maxBy = require('lodash.maxby');
 
-const model = tf.loadModel(resolve(__dirname, 'model.json'));
+const modelLoad = tf.loadModel(resolve(__dirname, 'model.json'));
 
-module.exports = async buffer => {
+module.exports = async img => {
+	const model = await modelLoad;
+	const predictedClass = tf.tidy(() => {
+		const predictions = model.predict(img);
+		return predictions.as1D().argMax();
+	});
 
+	const arr = await predictedClass.data();
 };
