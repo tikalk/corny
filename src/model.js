8040e429b2afd5792741721f36eae3211562b390
alonronin <alon@ronin.co.il>
2018-07-10 16:58:11 +0300

    Command selection algorithm

--- src/model.js
+++ src/model.js
@@ -4,14 +4,31 @@ require('@tensorflow/tfjs-node');
 const { resolve } = require('path');
 const maxBy = require('lodash.maxby');
 
+const canvas = require('canvas');
+
 const modelLoad = tf.loadModel(resolve(__dirname, 'model.json'));
 
-module.exports = async img => {
+const minProbability = 0.9;
+
+const commands = {
+	'0': undefined,
+	'1': 'corny.sayHello',
+	'2': 'corny.gitPush',
+	'3': 'corny.gitPull',
+};
+
+module.exports = async buffer => {
+	const img = 'magic'; // TODO
 	const model = await modelLoad;
 	const predictedClass = tf.tidy(() => {
 		const predictions = model.predict(img);
 		return predictions.as1D().argMax();
 	});
 
-	const arr = await predictedClass.data();
+	const arr = Object.entries(await predictedClass.data());
+	const selectedCommand = arr.filter(([key, prob]) => prob > minProbability).shift();
+	if (selectedCommand) {
+		const [key, prob] = selectedCommand;
+		return key;
+	}
 };
